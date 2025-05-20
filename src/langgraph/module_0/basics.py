0723fc0696e24e42669da42726a5e26cc822b35f
Florencio Paucar Sedano <florenciopaucar@gmail.com>
2025-05-21 01:43:52 +0200

    feat: added basic graph

--- src/langgraph/module_0/basics.py
+++ src/langgraph/module_0/basics.py
@@ -1,30 +1,24 @@
 from langchain_openai import ChatOpenAI
-gpt4o_chat = ChatOpenAI(model="gpt-4o", temperature=0)
-gpt35_chat = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
-
-
-
-
 from langchain_core.messages import HumanMessage
 
-# Create a message
-msg = HumanMessage(content="Hello world", name="Lance")
-
-# Message list
-messages = [msg]
-
-# Invoke the model with a list of messages 
-gpt4o_chat.invoke(messages)
-
-
-'''
+# Test GEMINI integration
 from langchain_google_genai import ChatGoogleGenerativeAI
-llm_gemini = ChatGoogleGenerativeAI(
+llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
     temperature=0,
     max_tokens=None,
     timeout=None,
     max_retries=2)
 
-print(f"Gemini model: {llm_gemini}")
-'''
+print(f"Gemini model: {llm}")
+msg = HumanMessage(content="Hello world", name="Lance")
+messages = [msg]
+res = llm.invoke(messages)
+print(res.content)
+
+# Test Tavily integration   
+from langchain_community.tools.tavily_search import TavilySearchResults
+tavily_search = TavilySearchResults(max_results=3)
+search_docs = tavily_search.invoke("What is LangGraph?")
+print(search_docs)
+
