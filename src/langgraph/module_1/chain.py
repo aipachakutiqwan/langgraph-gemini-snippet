895b56f028a922c37eb59a985e160b6a7c07419d
Florencio Paucar Sedano <florenciopaucar@gmail.com>
2025-05-25 02:01:16 +0200

    feat: added router

---
+++ src/langgraph/module_1/chain.py
@@ -0,0 +1,69 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph import MessagesState
+from langchain_core.messages import AIMessage, HumanMessage
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+
+messages = [AIMessage(content=f"So you said you were researching ocean mammals?", name="Model")]
+messages.append(HumanMessage(content=f"Yes, that's right.",name="Florentino"))
+messages.append(AIMessage(content=f"Great, what would you like to learn about.", name="Model"))
+messages.append(HumanMessage(content=f"I want to learn about the best place to see Orcas in the US.", name="Florentino"))
+for m in messages:
+    m.pretty_print()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2)
+
+result = llm.invoke(messages)
+print(f"type(result): {type(result)}")
+print(f"result: {result}")
+print(f"result.response_metadata: {result.response_metadata}")
+
+
+def multiply(a: int, b: int) -> int:
+    """Multiply a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+llm_with_tools = llm.bind_tools([multiply])
+tool_call = llm_with_tools.invoke([HumanMessage(content=f"What is 2 multiplied by 3", name="Florentino")])
+print(f"tool_call.tool_calls: ", tool_call.tool_calls)
+
+
+class MessagesState(MessagesState):
+    # Add any keys needed beyond messages, which is pre-built 
+    pass
+# Initial state
+initial_messages = [AIMessage(content="Hello! How can I assist you?", name="Model"),
+                    HumanMessage(content="I'm looking for information on marine biology.", name="Florentino")
+                   ]
+# New message to add
+new_message = AIMessage(content="Sure, I can help with that. What specifically are you interested in?", name="Model")
+# Test
+add_messages(initial_messages , new_message)
+# Node
+def tool_calling_llm(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_edge(START, "tool_calling_llm")
+builder.add_edge("tool_calling_llm", END)
+graph = builder.compile()
+# Print graph
+print(f"graph.get_graph: {graph.get_graph()}")
+# Invoke the graph
+messages = graph.invoke({"messages": HumanMessage(content="Hello!")})
+for m in messages['messages']:
+    m.pretty_print()
+# Invoke the graph
+messages = graph.invoke({"messages": HumanMessage(content="Multiply 2 and 3")})
+for m in messages['messages']:
+    m.pretty_print()
---
+++ src/langgraph/module_1/router.py
@@ -0,0 +1,50 @@
+from langgraph.prebuilt import ToolNode
+from langgraph.prebuilt import tools_condition
+from langgraph.graph.message import add_messages
+from langgraph.graph import MessagesState
+from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2)
+
+def multiply(a: int, b: int) -> int:
+    """Multiply a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+llm_with_tools = llm.bind_tools([multiply])
+print(f"llm_with_tools: {llm_with_tools}")
+# Node
+def tool_calling_llm(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_node("tools", ToolNode([multiply]))
+builder.add_edge(START, "tool_calling_llm")
+builder.add_conditional_edges(
+    "tool_calling_llm",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", END)
+graph = builder.compile()
+print(f"graph.get_graph: {graph.get_graph()}")
+conversational_flow = [AIMessage(content="Hello! How can I assist you?", name="Model"),
+                       HumanMessage(content="I'm looking for information some useful information.", name="Florentino"),
+                       AIMessage(content="Sure, I can help with that. What specifically are you interested in?", name="Model"),
+                       HumanMessage(content="What is 3 multiplied by 2?" , name="Florentino")
+                       ]
+messages = graph.invoke({"messages": conversational_flow})
+for m in messages['messages']:
+    m.pretty_print()
