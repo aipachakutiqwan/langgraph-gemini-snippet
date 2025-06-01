27f1a4614b59ce90f01e9d4ef36e95365c292b0d
Florencio Paucar Sedano <florenciopaucar@gmail.com>
2025-06-02 01:09:20 +0200

    fix: linting code

--- src/langgraph/ux_human_in_the_loop/edit_state_human_feedback.py
+++ src/langgraph/ux_human_in_the_loop/edit_state_human_feedback.py
@@ -7,6 +7,7 @@ from langgraph.graph import START, StateGraph
 from langgraph.prebuilt import tools_condition, ToolNode
 from langchain_core.messages import HumanMessage, SystemMessage
 
+
 # EDITING STATE
 def multiply(a: int, b: int) -> int:
     """Multiply a and b.
@@ -16,6 +17,7 @@ def multiply(a: int, b: int) -> int:
         b: second int
     """
     return a * b
+
 
 # This will be a tool
 def add(a: int, b: int) -> int:
@@ -27,6 +29,7 @@ def add(a: int, b: int) -> int:
     """
     return a + b
 
+
 def divide(a: int, b: int) -> float:
     """Divide a by b.
 
@@ -36,20 +39,28 @@ def divide(a: int, b: int) -> float:
     """
     return a / b
 
+
 tools = [add, multiply, divide]
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
     temperature=0,
     max_tokens=None,
     timeout=None,
-    max_retries=2)
+    max_retries=2,
+)
 llm_with_tools = llm.bind_tools(tools)
 
 # System message
-sys_msg = SystemMessage(content="You are a helpful assistant tasked with performing arithmetic on a set of inputs.")
+sys_msg = SystemMessage(
+    content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+)
+
+
 # Node
 def assistant(state: MessagesState):
-   return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+    return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+
+
 # Graph
 builder = StateGraph(MessagesState)
 # Define nodes: these do the work
@@ -75,7 +86,7 @@ initial_input = {"messages": "Multiply 2 and 3"}
 thread = {"configurable": {"thread_id": "1"}}
 # Run the graph until the first interruption
 for event in graph.stream(initial_input, thread, stream_mode="values"):
-    event['messages'][-1].pretty_print()
+    event["messages"][-1].pretty_print()
 state = graph.get_state(thread)
 print(f"state: {state}")
 # update the state with a new message
@@ -84,16 +95,17 @@ graph.update_state(
     {"messages": [HumanMessage(content="No, actually multiply 3 and 3!")]},
 )
 new_state = graph.get_state(thread).values
-for m in new_state['messages']:
+for m in new_state["messages"]:
     m.pretty_print()
 # Continue the graph from the updated state
 for event in graph.stream(None, thread, stream_mode="values"):
-    event['messages'][-1].pretty_print()
+    event["messages"][-1].pretty_print()
 # let continue to the result
 for event in graph.stream(None, thread, stream_mode="values"):
-    event['messages'][-1].pretty_print()
+    event["messages"][-1].pretty_print()
 
-# EDITING GRAPH STATE IN STUDIO 
+# EDITING GRAPH STATE IN STUDIO
+
 
 async def edit_state():
     # This is the URL of the local development server
@@ -108,48 +120,62 @@ async def edit_state():
         interrupt_before=["assistant"],
     ):
         print(f"Receiving new event of type: {chunk.event}")
-        messages = chunk.data.get('messages', [])
+        messages = chunk.data.get("messages", [])
         if messages:
             print(messages[-1])
         print("-" * 50)
-    current_state = await client.threads.get_state(thread['thread_id'])
+    current_state = await client.threads.get_state(thread["thread_id"])
     print(f"Current state: {current_state}")
-    last_message = current_state['values']['messages'][-1]
+    last_message = current_state["values"]["messages"][-1]
     print(f"Last message: {last_message}")
-    last_message['content'] = "No, actually multiply 3 and 3!"
+    last_message["content"] = "No, actually multiply 3 and 3!"
     print(f"Last message edited: {last_message}")
-    await client.threads.update_state(thread['thread_id'], {"messages": last_message})
-    async for chunk in client.runs.stream(thread["thread_id"],
-                                          assistant_id="agent",
-                                          input=None,
-                                          stream_mode="values",
-                                          interrupt_before=["assistant"]):
+    await client.threads.update_state(thread["thread_id"], {"messages": last_message})
+    async for chunk in client.runs.stream(
+        thread["thread_id"],
+        assistant_id="agent",
+        input=None,
+        stream_mode="values",
+        interrupt_before=["assistant"],
+    ):
         print(f"Stream 1 receiving new event of type: {chunk.event}")
-        messages = chunk.data.get('messages', [])
+        messages = chunk.data.get("messages", [])
         if messages:
             print(messages[-1])
         print("-" * 50)
-    async for chunk in client.runs.stream(thread["thread_id"],
-                                          assistant_id="agent",
-                                          input=None,
-                                          stream_mode="values",
-                                          interrupt_before=["assistant"]):
+    async for chunk in client.runs.stream(
+        thread["thread_id"],
+        assistant_id="agent",
+        input=None,
+        stream_mode="values",
+        interrupt_before=["assistant"],
+    ):
         print(f"Stream 2 receiving new event of type: {chunk.event}")
-        messages = chunk.data.get('messages', [])
+        messages = chunk.data.get("messages", [])
         if messages:
             print(messages[-1])
         print("-" * 50)
-#asyncio.run(edit_state())
+
+
+# asyncio.run(edit_state())
 
 # AWAITING USER INPUT
 # System message
-sys_msg = SystemMessage(content="You are a helpful assistant tasked with performing arithmetic on a set of inputs.")
+sys_msg = SystemMessage(
+    content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+)
+
+
 # no-op node that should be interrupted on
 def human_feedback(state: MessagesState):
     pass
+
+
 # Assistant node
 def assistant(state: MessagesState):
-   return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+    return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+
+
 # Graph
 builder = StateGraph(MessagesState)
 # Define nodes: these do the work
@@ -186,6 +212,3 @@ for event in graph.stream(None, thread, stream_mode="values"):
 # Continue the graph execution
 for event in graph.stream(None, thread, stream_mode="values"):
     event["messages"][-1].pretty_print()
-
-
-
