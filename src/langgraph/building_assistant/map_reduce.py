5eef7f6b516112e90e4454d125b562f2f408cd2c
Florencio Paucar Sedano <florenciopaucar@gmail.com>
2025-06-07 14:35:10 +0200

    feat: added map reduce example

--- src/langgraph/building_assistant/map_reduce.py
+++ src/langgraph/building_assistant/map_reduce.py
@@ -1,7 +1,8 @@
+import json
+from langchain_google_genai import ChatGoogleGenerativeAI
 import operator
 from typing import Annotated
 from typing_extensions import TypedDict
-from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic import BaseModel
 from langgraph.constants import Send
 from langgraph.graph import END, StateGraph, START
@@ -19,8 +20,7 @@ model = ChatGoogleGenerativeAI(
     timeout=None,
     max_retries=2,
 )
-
-# Define the state
+# Parallelizing joke generation
 class Subjects(BaseModel):
     subjects: list[str]
 
@@ -38,6 +38,10 @@ def generate_topics(state: OverallState):
     response = model.with_structured_output(Subjects).invoke(prompt)
     return {"subjects": response.subjects}
 
+def continue_to_jokes(state: OverallState):
+    return [Send("generate_joke", {"subject": s}) for s in state["subjects"]]
+
+# Joke generation (map)
 class JokeState(TypedDict):
     subject: str
 
@@ -49,24 +53,28 @@ def generate_joke(state: JokeState):
     response = model.with_structured_output(Joke).invoke(prompt)
     return {"jokes": [response.joke]}
 
+# Best joke selection (reduce)
 def best_joke(state: OverallState):
     jokes = "\n\n".join(state["jokes"])
     prompt = best_joke_prompt.format(topic=state["topic"], jokes=jokes)
     response = model.with_structured_output(BestJoke).invoke(prompt)
     return {"best_selected_joke": state["jokes"][response.id]}
 
-def continue_to_jokes(state: OverallState):
-    return [Send("generate_joke", {"subject": s}) for s in state["subjects"]]
 
 # Construct the graph: here we put everything together to construct our graph
-graph_builder = StateGraph(OverallState)
-graph_builder.add_node("generate_topics", generate_topics)
-graph_builder.add_node("generate_joke", generate_joke)
-graph_builder.add_node("best_joke", best_joke)
-graph_builder.add_edge(START, "generate_topics")
-graph_builder.add_conditional_edges("generate_topics", continue_to_jokes, ["generate_joke"])
-graph_builder.add_edge("generate_joke", "best_joke")
-graph_builder.add_edge("best_joke", END)
+graph = StateGraph(OverallState)
+graph.add_node("generate_topics", generate_topics)
+graph.add_node("generate_joke", generate_joke)
+graph.add_node("best_joke", best_joke)
+graph.add_edge(START, "generate_topics")
+graph.add_conditional_edges("generate_topics", continue_to_jokes, ["generate_joke"])
+graph.add_edge("generate_joke", "best_joke")
+graph.add_edge("best_joke", END)
 
 # Compile the graph
-graph = graph_builder.compile()
+app = graph.compile()
+print(json.dumps(app.get_graph().to_json(), indent=2))
+
+# Call the graph: here we call it to generate a list of jokes
+for s in app.stream({"topic": "animals"}):
+    print(s)
